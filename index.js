0feaedecfc07c67d91ac5654e976ac74b36aa952
Cebastienemeka <cebastienemeka@gmail.com>
2023-06-07 22:24:59 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+// Sidebar
+const menuItems = document.querySelectorAll('.menu-item');
+
+// Messgaes
+const messagesNotification = document.querySelector('#messages-notification');
+const messages = document.querySelector('.messages');
+const message = document.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search')
+
+// Sidebar
+
+// remove all active class from all menu items
+const changeActiveItem = () => {
+    menuItems.forEach(item => {
+        item.classList.remove('active');    
+    })
+}
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        changeActiveItem();
+        item.classList.add('active');
+        if(item.id != 'notifications') {
+            document.querySelector('.notifications-popup').
+            style.display = 'none';
+        }
+        else{
+            document.querySelector('.notifications-popup').
+            style.display = 'block';
+            document.querySelector('#notifications .notification-count').
+            style.display = 'none';
+        }
+    })
+})
+
+// Messages
+// searches chats
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase(); 
+    message.forEach(user => {
+        let name = user.querySelector('h5').textContent.toLowerCase();
+        if(name.indexOf(val) != -1) {
+            user.style.display= 'flex';
+        }
+        else{
+            user.style.display = 'none';
+        }
+    })
+}
+
+//search chat
+messageSearch.addEventListener('keyup', searchMessage);
+
+// hightlight messages card when messages menu item is clicked 
+messagesNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('.notification-count').
+    style.display = 'none'
+    setTimeout(() => {
+        messages.style.boxShadow = 'none'
+    }, 2000)
+})
+
+
+// Theme/Display Customization
